7a997fbed91714725a4fa5bc4663f3f763327c23
haning_nanda <haningnandahapsari@gmail.com>
2020-09-18 13:52:25 +0700

    splgauss

--- src/main.java
+++ src/main.java
@@ -17,11 +17,21 @@ public class main {
         if (pilihan==1) {
             System.out.println("Pilih Metode yang Ingin Anda Gunakan :");
             System.out.println("1. Metode eliminasi Gauss\n2. Metode eliminasi Gauss-Jordan\n3. Metode matriks balikan\n4. Kaidah Cramer");
+            int pilihan1;
+            System.out.println("Pilih Metode :");
+            pilihan1 = menu.nextInt();
+            if (pilihan1==1) {
+                Program menu_1_1= new Program();
+                menu_1_1.splgauss();
+            }
         }
-    }
+    } 
 }
 class Program{
     public void splgauss(){
-        System.out.println("Hello Haning");
+        int matriks;
+        Scanner menprog = new Scanner (System.in);
+        System.out.println("Masukkan ukuran matriks : ");
+        matriks = menprog.nextInt();
     }
 }
